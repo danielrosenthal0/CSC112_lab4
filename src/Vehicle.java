350f24269892f970f9bcb61f3e620abc92f8ed4e
danielrosenthal2 <rosedb19@wfu.edu>
2021-03-14 16:29:41 -0400

    commit 1

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -15,9 +15,23 @@ public class Vehicle {
 
 
     //1) create Vehicle constructor
+    public Vehicle() {
+        String Vtag;
+        String Vvin;
+        double Vpurchaseprice;
+        int Vpurchaseyear;
+    }
 
 
     //2) create getters and setters for the data memebers get tag
+    //tag getter
+    public String getVtag() {
+        return Vtag;
+    }
+    //tag setter
+    public void setVtag(String newTag) {
+        this.Vtag = newTag;
+    }
      //2.1) getTax should compute the basic vehicle tax based on purchase price
 
     //3) override toString method to print the vehicle info
