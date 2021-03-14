2c21f7b5dd8f61fe710c5ba9090d7dcfa5c1d67a
danielrosenthal2 <rosedb19@wfu.edu>
2021-03-14 17:04:12 -0400

    commit 2

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -6,13 +6,15 @@
 
 public class Vehicle {
 
+
+
     public static enum Venum  {car, truck, motorcycle, moped};
 
     protected String Vtag;
     protected String Vvin;
     protected double Vpurchaseprice;
     protected int  Vpurchaseyear;
-
+    private double tax;
 
     //1) create Vehicle constructor
     public Vehicle() {
@@ -20,6 +22,7 @@ public class Vehicle {
         String Vvin;
         double Vpurchaseprice;
         int Vpurchaseyear;
+        double tax;
     }
 
 
@@ -33,7 +36,20 @@ public class Vehicle {
         this.Vtag = newTag;
     }
      //2.1) getTax should compute the basic vehicle tax based on purchase price
+    public double getTax() {
+        double tax = 0.0;
+        tax = (0.055*Vpurchaseprice)*(2021-Vpurchaseyear);
+        return tax;
+    }
+    public void setTax(double newTax) {
+        this.tax = newTax;
+    }
 
     //3) override toString method to print the vehicle info
+//print method
+    @Override
+    public String toString() {
 
+        return("The vehicle's tag is " + Vtag + " and the tax on the vehicle is " + tax);
+    }
 }
