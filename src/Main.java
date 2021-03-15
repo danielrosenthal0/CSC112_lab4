729deda6c9c7231c70a81d67fa4bb33fb2643b67
danielrosenthal2 <rosedb19@wfu.edu>
2021-03-14 23:22:59 -0400

    commit 6, inventory is not printing

--- src/Main.java
+++ src/Main.java
@@ -7,40 +7,45 @@ public class Main {
 
     public static void main(String[] args) {
         //1) create an inventory of type ArrayList of vehicles
-        ArrayList<Vehicle> inventory = new ArrayList<Vehicle>();
+        ArrayList<String> inventory = new ArrayList<>();
 
 
         //2) create an input file of Vehicles,Cars andTrucks in comma separated values (CSV) format
-//        fileoutputstram
-//                fileprinter printwriter
-//                        "vehicle", tag,
+
 
         FileOutputStream fos = null;
         File csvFile = new File("src/inventory.csv");
         try {
             csvFile.createNewFile();
             PrintWriter writer = new PrintWriter(csvFile);
-            writer.println("Car");
+            writer.println("Car, bmt147, lkjlkjlklj12231231lkjl, 15173.17, 2005, pinto, true, 4");
+            writer.println("Truck, abc698, iuyj08-9j, 35165.88, 2019, 4");
             writer.close();
         }
         catch (IOException e) {
             System.exit(1);
         }
 
-//            try {
-//                fos = new FileOutputStream("inventory.csv");
-//            }
-//            catch (FileNotFoundException e) {
-//                System.out.println("File is not found.");
-//                System.exit(1);
-//            }
-
-//        FileInputStream inventory = new FileInputStream("inventory.csv");
-//        Scanner inventory = new Scanner(inventory);
-
-
         //3) put vehicles in the inventory (read from a file and add), the followin lines are examples
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream("inventory.csv");
+        } catch (FileNotFoundException e){
+            System.exit(1);
+        }
+        Scanner reader = new Scanner(fis);
+        reader.useDelimiter(",");
+        while (reader.hasNextLine()) {
+            String currentLine = reader.nextLine();
+            inventory.add(currentLine);
+        }
+        reader.close();
 
+        //testing inventory
+        int size = inventory.size();
+        for (int i = 0; i < size; i++) {
+            System.out.println(inventory.get(i));
+        }
 //        inventory.add(new Car("bmt147", "lkjlkjlklj12231231lkjl", 15173.17, 2005,
 //                "pinto", true, 4));
 //        inventory.add(new Vehicle("xrc761","oojojjpjp09809",3500.00, 2015));
