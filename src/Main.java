bd596d297480d4df0442e77d751e25b395c067d3
danielrosenthal2 <rosedb19@wfu.edu>
2021-03-14 22:39:25 -0400

    commit 5, created inventory csv

--- src/Main.java
+++ src/Main.java
@@ -7,30 +7,44 @@ public class Main {
 
     public static void main(String[] args) {
         //1) create an inventory of type ArrayList of vehicles
-        ArrayList<Vehicle> inventory = new ArrayList<Vehicle>;
+        ArrayList<Vehicle> inventory = new ArrayList<Vehicle>();
 
 
         //2) create an input file of Vehicles,Cars andTrucks in comma separated values (CSV) format
 //        fileoutputstram
 //                fileprinter printwriter
 //                        "vehicle", tag,
-        FileOutputStream fos = null;
-        File csvFile;
-        public void creatingFile() {
-            try {
 
-            }
+        FileOutputStream fos = null;
+        File csvFile = new File("src/inventory.csv");
+        try {
+            csvFile.createNewFile();
+            PrintWriter writer = new PrintWriter(csvFile);
+            writer.println("Car");
+            writer.close();
         }
-        FileInputStream inventory = new FileInputStream("inventory.csv");
-        Scanner inventory = new Scanner(inventory);
+        catch (IOException e) {
+            System.exit(1);
+        }
+
+//            try {
+//                fos = new FileOutputStream("inventory.csv");
+//            }
+//            catch (FileNotFoundException e) {
+//                System.out.println("File is not found.");
+//                System.exit(1);
+//            }
+
+//        FileInputStream inventory = new FileInputStream("inventory.csv");
+//        Scanner inventory = new Scanner(inventory);
 
 
         //3) put vehicles in the inventory (read from a file and add), the followin lines are examples
 
-        inventory.add(new Car("bmt147", "lkjlkjlklj12231231lkjl", 15173.17, 2005,
-                "pinto", true, 4));
-        inventory.add(new Vehicle("xrc761","oojojjpjp09809",3500.00, 2015));
-        inventory.add(new Truck("abc698", "iuyj08-9j", 35165.88, 2019, 4));
+//        inventory.add(new Car("bmt147", "lkjlkjlklj12231231lkjl", 15173.17, 2005,
+//                "pinto", true, 4));
+//        inventory.add(new Vehicle("xrc761","oojojjpjp09809",3500.00, 2015));
+//        inventory.add(new Truck("abc698", "iuyj08-9j", 35165.88, 2019, 4));
 
 
         //4) for each vehicle in the inventory print the tag and the tax
